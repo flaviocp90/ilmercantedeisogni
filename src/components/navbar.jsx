85f26d34da255e27838b9939a88aaed228fe38fe
flaviocp90 <flaviocoppola90@gmail.com>
2021-11-12 00:12:33 +0100

    minor update navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,9 +4,8 @@ import '../style/App.css';
 const Navbar = () => {
     return (
         <div className='Navbar container'>
-            <h3><a href='/'>Il Mercante dei Sogni</a></h3>
+            <h3 className='d-flex'><a href='/'>Il Mercante dei Sogni</a></h3>
             <ul className='list-item'>
-
                 <li><a href="/about">About</a></li>
                 <li><a href="/vetrina">Vetrina</a></li>
             </ul>
